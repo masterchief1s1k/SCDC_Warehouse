c8e9a4504beffd0cf1e7ee8ea40a207e5e3f08ff
trongnghia159 <trongnghia.le159@gmail.com>
2018-01-28 22:32:16 +0700

    init commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html >
+<?php include_once 'widgets/head.php' ?>
+<body>
+<?php
+  //MENU BAR
+  include_once 'modules/mod_menu.php';
+  //INTRO W IMAGE BACKGROUND
+  include_once 'modules/mod_intro.php';
+  //BRIEF INTRODUCTION
+  include_once 'modules/mod_brief_discuss.php';
+  //FEATURES
+  include_once 'modules/mod_features.php';
+  //PRICING TABLE
+  include_once 'modules/mod_pricing.php';
+  //FOOTER
+  include_once 'widgets/footer.php';
+  //SCRIPT EBBEDED
+  include_once 'widgets/script.php';
+?>
+  <input name="animation" type="hidden">
+  </body>
+</html>
